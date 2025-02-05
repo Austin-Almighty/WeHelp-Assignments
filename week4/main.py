06fd245d022e3dbce7cd27091f51d64be7867062
tim479863 <131895709+tim479863@users.noreply.github.com>
2025-02-04 21:29:10 -0800

    Task 1 Partial Complete

--- week4/main.py
+++ week4/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, Request, Path, Query
+from fastapi.responses import FileResponse, RedirectResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from typing import Annotated
+
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return "This is the index"
+
+
+# @app.get("/user")
+templates = Jinja2Templates(directory="static")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
+
+
 
 
 
