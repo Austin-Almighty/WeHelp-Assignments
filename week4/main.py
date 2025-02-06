1e317064a77e86d6b8f93ce17d8d9568f34e0b68
tim479863 <131895709+tim479863@users.noreply.github.com>
2025-02-06 01:59:05 -0800

    Task 4 Complete

--- week4/main.py
+++ week4/main.py
@@ -12,8 +12,12 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    # return templates.TemplateResponse("index.html", context={"request": request})
-    return FileResponse("index.html")
+    return templates.TemplateResponse("index.html", context={"request": request})
+
+@app.get("/square/{positive}")
+def sqaure(positive:Annotated[int, None], request: Request):
+    result = int(positive) * int(positive)
+    return templates.TemplateResponse('base.html', context={"request": request, "page_title": "正整數平方計算結果", "message": result})
 
 
 VALID_USERNAME = 'test'
@@ -21,19 +25,28 @@ VALID_PASSWORD = 'test'
 
 @app.post('/signin')
 async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username, "password": password}
-    # if username == VALID_USERNAME and password == VALID_PASSWORD:
-    #     return RedirectResponse("/member")
-    # else:
-    #     return RedirectResponse("/error")
+    if len(username.strip()) == 0 or len(password.strip()) == 0:
+        return RedirectResponse(url="/error?message=Please+enter+username+and+password", status_code=303)
+    elif username == VALID_USERNAME and password == VALID_PASSWORD:
+        return RedirectResponse(url="/member", status_code=303)
+    else:
+        return RedirectResponse(url="/error?message=Username+or+password+is+not+correct", status_code=303)
+
 
 @app.get('/member', response_class=HTMLResponse)
 async def success(request: Request):
     return templates.TemplateResponse("base.html", context={"request": request, "page_title": "歡迎光臨，這是會員頁 ", "message": "恭喜您，成功登入系統"})
 
+
 @app.get('/error', response_class=HTMLResponse)
-async def error(request: Request):
-    return templates.TemplateResponse("base.html", context={"request": request, "page_title": "失敗頁面", "message": "Invalid username or password. Please try again."})
+async def error(request: Request, message: str = Query(default="登入失敗")):
+    return templates.TemplateResponse("base.html",
+        context={
+            "request": request,
+            "page_title": "失敗頁面",
+            "message": message
+        }
+    )
 
 
 
