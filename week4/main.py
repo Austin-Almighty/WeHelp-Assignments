06e47cb3173bdfacd293788504abcbb3b76c9c99
tim479863 <131895709+tim479863@users.noreply.github.com>
2025-02-05 16:24:55 -0800

    new commit

--- week4/main.py
+++ week4/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, Path, Query, Form
-from fastapi.responses import FileResponse, RedirectResponse, HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Annotated
@@ -12,40 +12,29 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", context={"request": request})
+    # return templates.TemplateResponse("index.html", context={"request": request})
+    return FileResponse("index.html")
+
+
+VALID_USERNAME = 'test'
+VALID_PASSWORD = 'test'
+
+@app.post('/signin')
+async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username, "password": password}
+    # if username == VALID_USERNAME and password == VALID_PASSWORD:
+    #     return RedirectResponse("/member")
+    # else:
+    #     return RedirectResponse("/error")
+
+@app.get('/member', response_class=HTMLResponse)
+async def success(request: Request):
+    return templates.TemplateResponse("base.html", context={"request": request, "page_title": "歡迎光臨，這是會員頁 ", "message": "恭喜您，成功登入系統"})
 
 @app.get('/error', response_class=HTMLResponse)
-async def error(
-    request: Request,
-):
-    context = {
-        "request": request,
-        "page_title": "失敗頁面",
-        "message": "missing info"
-    }
-    return templates.TemplateResponse('base.html', context=context)
+async def error(request: Request):
+    return templates.TemplateResponse("base.html", context={"request": request, "page_title": "失敗頁面", "message": "Invalid username or password. Please try again."})
 
-
-@app.post('/signin/')
-async def signin(username:Annotated[str, Form()], password:Annotated[str, Form()]):
-    if len(username) == 0 or len(password) == 0:
-        # context = {'request': request,
-        #            "page_title": "失敗頁面",
-        #            "message": "Please enter your username and password"
-        # }
-        return RedirectResponse(url="/error")
-    # elif username == "test" and password == "test":
-    #     return RedirectResponse('/member')
-    # else:
-    #     return RedirectResponse('/error')
-
-
-# @app.get('/error')
-# def error(message):
-#     if 
-
-# @app.get('/member')
-# def 
 
 
 
