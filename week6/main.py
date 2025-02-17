173e67b773dc695dafa036a4bb57ed4f00d0abf2
tim479863 <131895709+tim479863@users.noreply.github.com>
2025-02-17 15:33:44 -0800

    Week 6 Updates

--- week6/main.py
+++ week6/main.py
@@ -4,9 +4,23 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import Annotated
 from starlette.middleware.sessions import SessionMiddleware
+from config import config
+import mysql.connector
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key='WeHelp')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+connection = mysql.connector.connect(**config)
+
+cursor = connection.cursor()
+cursor.execute
+connection.commit()
+
+connection.close()
+
+
+@app.post()
+def signin()
