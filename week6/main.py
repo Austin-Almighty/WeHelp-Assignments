a3193ddfa8c744e808dc661e3a0e3be541a97c8a
tim479863 <131895709+tim479863@users.noreply.github.com>
2025-02-17 22:38:42 -0800

    Task 1

--- week6/main.py
+++ week6/main.py
@@ -13,14 +13,41 @@ app.add_middleware(SessionMiddleware, secret_key='WeHelp')
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-connection = mysql.connector.connect(**config)
+# connection = mysql.connector.connect(**config)
+# cursor = connection.cursor()
+# cursor.execute
+# connection.commit()
 
-cursor = connection.cursor()
-cursor.execute
-connection.commit()
+# connection.close()
 
-connection.close()
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
 
+@app.get('/member')
+def success(request: Request):
+    return templates.TemplateResponse('base.html', context={'request': request, 'page_title': '歡迎光臨，這是會員頁', 'message': 'Austin，歡迎登入系統'})
 
-@app.post()
-def signin()
+@app.get('/error')
+def error(request: Request, message: str = Query(default="登入失敗")):
+    return templates.TemplateResponse("base.html",
+        context={
+            "request": request,
+            "page_title": "失敗頁面",
+            "message": message
+        }
+    )
+
+@app.post('/signup')
+def signup(request: Request):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute("")
+
+@app.post('signin')
+def signin():
+
+@app.get('/signout')
+def signout(request: Request):
+    request.session["SIGNED-IN"] = False
+    return RedirectResponse(url="/")
---
+++ week6/static/scripts.js
@@ -0,0 +1,28 @@
+const register_form = document.getElementById("register");
+const register_name = document.getElementById('register_name');
+const register_username = document.getElementById("register_username");
+const register_password = document.getElementById('register_password');
+const register_button = document.getElementById('register_button');
+
+
+const login_form = document.getElementById("login");
+const login_username = document.getElementById('login_username');
+const login_password = document.getElementById('login_password');
+const login_button = document.getElementById('login_button');
+
+register_button.addEventListener("click", () => {
+    if (!register_name.value.trim() || !register_password.value.trim() || !register_username.value.trim()) {
+        alert("註冊前，請確實輸入姓名、帳號、密碼")
+    } else {
+        register_form.submit();
+    }
+})
+
+login_button.addEventListener("click", () => {
+    if (!login_username.value.trim() || !login_password.value.trim()) {
+        alert("請輸入")
+    } else {
+        login_form.submit();
+    }
+})
+
